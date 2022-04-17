e6eb4c1fa7744253353c62b895389cfce88db35d
devb0x <moniezgeoffrey59@gmail.com>
2022-04-18 00:40:36 +0200

    add proptypes

--- src/lib/components/Modal.jsx
+++ src/lib/components/Modal.jsx
@@ -1,39 +1,59 @@
 import React, {Fragment} from 'react'
+import PropTypes from "prop-types"
 
 import classes from './Modal.module.css'
 
 function Modal(props) {
-  const title = "Successfully Added"
-  const textMessage = "The new employee was created !"
 
   return (
-
     <Fragment>
       {props.show &&
-        <Fragment>
+        <div
+          className={`${classes['background']}`}
+          onClick={props.close}
+          data-testid="background"
+        >
           <div
             className={`${classes['modal']}`}
             style={{padding: props.padding}}
+            data-testid={"modal"}
           >
-            {props.closeBtn &&
+            {props.closeBtn}
+            {!props.closeBtn &&
               <header
                 className={`${classes['modal-btn__close']}`}
                 onClick={props.close}
+                data-testid="close-icon"
               >
-                <span className={`${classes['x']}`}>&times;</span>
+                <span
+                  className={`${classes['x']}`}
+                >
+                  &times;
+                </span>
               </header>
             }
-            <h3 className={`${classes['modal-title']}`}>{title}</h3>
-            <p className={`${classes['modal-text']}`}>{textMessage}</p>
+            <h3
+              className={`${classes['modal-title']}`}
+              data-testid="title"
+            >
+              {props.title}
+            </h3>
+            <p className={`${classes['modal-text']}`} data-testid="message">
+              {props.textMessage}
+            </p>
           </div>
-          <div
-            className={`${classes['background']}`}
-            onClick={props.close}
-          />
-        </Fragment>
+        </div>
       }
     </Fragment>
   )
 }
 
 export default Modal
+
+Modal.propTypes = {
+  show: PropTypes.bool,
+  close: PropTypes.func,
+  padding: PropTypes.string,
+  title: PropTypes.string,
+  textMessage: PropTypes.string
+}
